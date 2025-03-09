03fca5aa4f74f68c6dcf0ec3ef8bf3716f17ab74
nelsonthelad <nelsongdaniels@gmail.com>
2025-03-08 22:03:35 -0500

    Implemented IP Checking

--- netcheck.py
+++ netcheck.py
@@ -1,1 +1,50 @@
-print("Hello, World!")
+import speedtest
+import requests
+import socket
+
+# Checks if there is an internet connection
+def checkConnection():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False    
+
+# Gets the local IP address
+def getLocalIp():
+    try:
+        local_ip = socket.gethostbyname(socket.gethostname())
+    except:
+        local_ip = "Error"
+    return local_ip
+
+# Gets the public IP address
+def getPublicIp():
+    try:
+        public_ip = requests.get("https://api.ipify.org?format=json").json()["ip"]
+    except:
+        public_ip = "Error"
+    return public_ip
+
+# Main function
+def main():
+    print("Checking Internet Connection...")
+
+    if not checkConnection():
+        print("No internet connection")
+        return
+    
+    print("Internet Status: Connected")
+    print("Getting local IP address...")
+    local_ip = getLocalIp()
+    print(f"Local IP address: {local_ip}")
+
+    print("Getting public IP address...")
+    public_ip = getPublicIp()
+    print(f"Public IP address: {public_ip}")
+    
+    
+
+if __name__ == "__main__":
+    main()
