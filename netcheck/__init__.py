73d5e84366d22a55d6ce33ffd56e7fb49f226613
nelsonthelad <nelsongdaniels@gmail.com>
2025-03-09 17:02:32 -0400

    Created Package

---
+++ netcheck/__init__.py
@@ -0,0 +1,5 @@
+"""
+NetCheck - A tool for checking network speed and information.
+"""
+
+__version__ = '0.1.0' 
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netcheck",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "speedtest-cli",
+        "rich",
+    ],
+    entry_points={
+        "console_scripts": [
+            "netcheck=netcheck.main:cli",
+        ],
+    },
+    author="Nelson Daniels",
+    author_email="nelsongdaniels@gmail.com",
+    description="A shell command for network speed and information",
+    keywords="network, speed test, CLI",
+    url="https://github.com/nelsonthelad/netcheck",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+) 
